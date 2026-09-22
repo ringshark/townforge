fac3056b951c65a4d87ebed42ea32413bc6d4169
ringshark <mark.ring157@gmail.com>
2026-09-21 23:15:33 -0400

    Darken primary text/heading/accent colors for better readability

--- townforge.js
+++ townforge.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\markr\AppData\Local\Temp\tmpmfkz4wmz.js
+// include: C:\Users\markr\AppData\Local\Temp\tmp_wqohc9z.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -234,21 +234,21 @@ Module['FS_createPath']("/assets", "wilderness_props", true, true);
 
   })();
 
-// end include: C:\Users\markr\AppData\Local\Temp\tmpmfkz4wmz.js
-// include: C:\Users\markr\AppData\Local\Temp\tmpj9z0jozl.js
+// end include: C:\Users\markr\AppData\Local\Temp\tmp_wqohc9z.js
+// include: C:\Users\markr\AppData\Local\Temp\tmpe8wcx9t8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\markr\AppData\Local\Temp\tmpj9z0jozl.js
-// include: C:\Users\markr\AppData\Local\Temp\tmplndajm3f.js
+  // end include: C:\Users\markr\AppData\Local\Temp\tmpe8wcx9t8.js
+// include: C:\Users\markr\AppData\Local\Temp\tmpml70mt2c.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\markr\AppData\Local\Temp\tmplndajm3f.js
+  // end include: C:\Users\markr\AppData\Local\Temp\tmpml70mt2c.js
 
 
 var programArgs = [];
