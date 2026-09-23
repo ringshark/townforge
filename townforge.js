fcc0ef6f6b5bc2b75c15a84a646412341b218605
ringshark <mark.ring157@gmail.com>
2026-09-23 16:24:47 -0400

    Carl art integration Phase 1: generalize DirSpriteSheet/DrawActorSprite (no visible change)

--- townforge.js
+++ townforge.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\markr\AppData\Local\Temp\tmpk96u_2i9.js
+// include: C:\Users\markr\AppData\Local\Temp\tmparvd9dzs.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -234,21 +234,21 @@ Module['FS_createPath']("/assets", "wilderness_props", true, true);
 
   })();
 
-// end include: C:\Users\markr\AppData\Local\Temp\tmpk96u_2i9.js
-// include: C:\Users\markr\AppData\Local\Temp\tmpqrxm_b36.js
+// end include: C:\Users\markr\AppData\Local\Temp\tmparvd9dzs.js
+// include: C:\Users\markr\AppData\Local\Temp\tmp727xfdx8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\markr\AppData\Local\Temp\tmpqrxm_b36.js
-// include: C:\Users\markr\AppData\Local\Temp\tmpfy69feai.js
+  // end include: C:\Users\markr\AppData\Local\Temp\tmp727xfdx8.js
+// include: C:\Users\markr\AppData\Local\Temp\tmp36d95gkz.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\markr\AppData\Local\Temp\tmpfy69feai.js
+  // end include: C:\Users\markr\AppData\Local\Temp\tmp36d95gkz.js
 
 
 var programArgs = [];
